e64490cab231cf1e55ec5a90eb72973ebff6ea35
sanjivchaudhary416 <sanjivchaudhary416@gmail.com>
2021-10-21 12:56:07 +0545

    naavbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+          <div className=" p-8 pb-2 flex flex-wrap justify-between border-2 border-gray-400">
+              <div className="flex flex-wrap space-x-20">
+                  <div>
+                      <p className="text-3xl text-gray-700 font-bold rounded-lg">Goggle</p>
+                  </div>
+                  <div>
+                      <input className="h-8 w-64 rounded-2xl p-2  outline-none text-gray-600" type="text" placeholder="Search" />
+                  </div>
+              </div>
+              <div> 
+                  
+              </div>
+          </div>  
+        </>
+    )
+}
+
+export default Navbar
