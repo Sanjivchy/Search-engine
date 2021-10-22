6cff43f1d1af3e27386d37f82038625a894c5569
sanjivchaudhary416 <sanjivchaudhary416@gmail.com>
2021-10-22 11:37:49 +0545

    loading component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,18 @@
 import React , {useEffect} from 'react'
 import { useLocation } from 'react-router-dom';
 import ReactPlayer from 'react-player';
+import  Loading  from './Loading';
+
+import { useResultContext } from './contexts/ResultContextProvider';
+
+
 
 const Results = () => {
+const { Results, isLoading , getResults, searchTerm } =useResultContext();
+ const location = useLocation();
+
+ if(isLoading) return <Loading/>;
+
     return (
         <div>
             result
